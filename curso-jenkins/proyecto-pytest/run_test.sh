bc485730d5e68108a05a66b669200def427623b7
JoseDevSecOps <jlperezr23@ul.edu.co>
2025-10-28 00:12:12 -0500

    proyecto python cuarto commit

--- curso-jenkins/proyecto-pytest/run_test.sh
+++ curso-jenkins/proyecto-pytest/run_test.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 echo "ejecutando entorno virtual"
+if [!"-d venv" ]; then
+    python3 -m venv venv
+fi     
 source venv/bin/activate
 
 echo "instalando dependencias"
+pip install --upgrade pip
 pip install -r requeriments.txt
 
 echo "Ejecutando pruebas con pytest"
